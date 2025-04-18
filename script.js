756c57dd05c40f70618a508940e5a474602fdb8c
vnpta <hoangtran1483@gmail.com>
2025-04-18 23:37:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-import { hienThiSoLuong, tangSoLuongHoanThanh } from "./firebase-counter.js";
-
 document.addEventListener("DOMContentLoaded", function () {
-  // Hiển thị số lượt hoàn thành khảo sát
-  hienThiSoLuong();
-
   // Highlight selected answer
   document.querySelectorAll(".options label").forEach((label) => {
     label.addEventListener("click", function () {
@@ -58,16 +53,11 @@ document.addEventListener("DOMContentLoaded", function () {
     for (const [key, value] of Object.entries(scores)) {
       const scoreElement = document.createElement("div");
       scoreElement.classList.add("score-item");
-      scoreElement.innerHTML = `<strong>${key.toUpperCase()}:</strong> ${
-        value !== null ? value : "Chưa trả lời đủ câu hỏi"
-      }`;
+      scoreElement.innerHTML = `<strong>${key.toUpperCase()}:</strong> ${value !== null ? value : "Chưa trả lời đủ câu hỏi"}`;
       resultContainer.appendChild(scoreElement);
     }
 
     document.getElementById("thankYouMessage").style.display = "block";
-
-    // Tăng số lượt hoàn thành khảo sát
-    tangSoLuongHoanThanh();
   }
 
   // Handle calculate button click
